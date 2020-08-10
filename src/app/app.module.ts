7d54b393e4ab53e256a19ed1f2933785dd327e36
Peter <ripiter15@wp.pl>
2020-08-10 18:20:09 +0200

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PersonComponent } from './person/person.component';
-import { Com1Component } from './com1/com1.component';
 import { ClickComponent } from './click/click.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonComponent,
-    Com1Component,
     ClickComponent
   ],
   imports: [
